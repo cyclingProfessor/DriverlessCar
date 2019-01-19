7fbd975946305d3530a6d06ccad90728af4666c0
David Cohen <d.cohen@rhul.ac.uk>
2019-01-19 10:03:25 +0000

    Changed to tb6612FNG pololu motor controller, which is smaller but has a max stall current of about 3A, so is less robust than the L298N.  On the other hand it is MOSFET so provides (slightly) better braking.

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -11,20 +11,23 @@
 // 6 -Motor 2 Right
 
 // Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.
+// Change to using Pololo TB6612FNG Mosfet controller
 
 // Motor 1
 int dir1PinA = 7;
 int dir2PinA = 8;
 int speedPinA = 6; // Needs to be a PWM pin to be able to control motor speed
+int enablePin = 9; // Enable Motor control
 
 void setup() {  // Setup runs once per reset
   Serial.begin(115200);
 
-  //Define L298N Dual H-Bridge Motor Controller Pins
+  //Define TB6612FNG Dual H-Bridge Motor Controller Pins
 
   pinMode(dir1PinA, OUTPUT);
   pinMode(dir2PinA, OUTPUT);
   pinMode(speedPinA, OUTPUT);
+  pinMode(enablePin, OUTPUT);
 }
 
 void loop() {
@@ -43,15 +46,17 @@ void loop() {
         analogWrite(speedPinA, 100);//Sets speed variable via PWM
         digitalWrite(dir1PinA, LOW);
         digitalWrite(dir2PinA, HIGH);
+        digitalWrite(enablePin, HIGH);
         Serial.println("Motor 1 Forward"); // Prints out “Motor 1 Forward” on the serial monitor
         Serial.println("   "); // Creates a blank line printed on the serial monitor
         break;
 
-      case '2': // Motor 1 Stop (Freespin)
+      case '2': // Motor 1 Stop (Brake)
         analogWrite(speedPinA, 0);
-        digitalWrite(dir1PinA, LOW);
+        digitalWrite(dir1PinA, HIGH);
         digitalWrite(dir2PinA, HIGH);
-        Serial.println("Motor 1 Stop");
+        digitalWrite(enablePin, HIGH);
+        Serial.println("Motor 1 Brake");
         Serial.println("   ");
         break;
 
@@ -59,7 +64,26 @@ void loop() {
         analogWrite(speedPinA, 100);
         digitalWrite(dir1PinA, HIGH);
         digitalWrite(dir2PinA, LOW);
+        digitalWrite(enablePin, HIGH);
         Serial.println("Motor 1 Reverse");
+        Serial.println("   ");
+        break;
+
+      case '4': // Motor 1 Coast
+        digitalWrite(enablePin, LOW);
+        digitalWrite(speedPinA, LOW);
+        digitalWrite(dir1PinA, LOW);
+        digitalWrite(dir2PinA, LOW);
+        Serial.println("Motor 1 Coast");
+        Serial.println("   ");
+        break;
+
+      case '5': // Motor 1 Short Brake
+        digitalWrite(speedPinA, HIGH);
+        digitalWrite(dir1PinA, HIGH);
+        digitalWrite(dir2PinA, HIGH);
+        digitalWrite(enablePin, HIGH);
+        Serial.println("Motor 1 Short Brake");
         Serial.println("   ");
         break;
 
