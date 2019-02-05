690d6c6b2a4472b090a9274a8f6938d3c4fce339
David Cohen <d.cohen@rhul.ac.uk>
2019-02-05 19:04:50 +0000

    Now the Motor tester is set up to use the new EPS8266 (D1 Mini Lite) board whcih we will access using the serial pins.
    The sketch now correctly reports the angle from the built in quad encoder useing interupts on two pins.
    It presents itself as a Wifi access point : espCar, with password - "thisisnotaspoon"
    It then enters a read loop (192.168.4.1:3000)
    Use S(tatus) to get the status printed.
    Use R(ight) ,L(eft) and C(enter) to control truning,
    Use + (more forwards), - (more backwards) and 0 (brake) to control speed.
    
    Now compensates for hysteresis by overturning and returning.

--- Motor/Motor.ino
+++ Motor/Motor.ino
@@ -1,94 +1,175 @@
-//Code by Reichenstein7 (thejamerson.com)
+#include "Encoder.h"
+#include "Ticker.h"
+#include <ESP8266WiFi.h>
+#include "Servo.h"
+
+const char *ssid = "ESPcarAP";
+const char *password = "thisisnotaspoon";
+WiFiClient client;
 
 //Keyboard Controls:
 //
-// 1 -Motor 1 Left
-// 2 -Motor 1 Stop
-// 3 -Motor 1 Right
+// + -Motor more forwards
+// - -Motor more backwards
+// 0 -Motor stop
+// s - print stats
+// L - trun servo to Left
+// R - Turn Servo to Right
+// C - Centre servo
 //
-// 4 -Motor 2 Left
-// 5 -Motor 2 Stop
-// 6 -Motor 2 Right
 
 // Declare L298N Dual H-Bridge Motor Controller directly since there is not a library to load.
 // Change to using Pololo TB6612FNG Mosfet controller
 
 // Motor 1
-int dir1PinA = 7;
-int dir2PinA = 8;
-int speedPinA = 6; // Needs to be a PWM pin to be able to control motor speed
-int enablePin = 9; // Enable Motor control
+int dir1PinA = D5;
+int dir2PinA = D6;
+int speedPinA = D2; // Needs to be a PWM pin to be able to control motor speed
+int enablePin = D3; // Enable Motor control
+int steerPin = D8;
+int servoPower = D0;
+
+const int MAX_SERVO = 2000;
+const int MIN_SERVO = 1000;
+const int MIN_ANGLE = 10; //neutral is 55 degrees
+const int NEUTRAL_ANGLE = 90;
+const int MAX_ANGLE = 105;
+
+volatile int speedEstimate = 0;
+volatile int lastPosition = 0;
+volatile unsigned long lastTime = 0L;
+int diffP = 0;
+int diffT = 0;
+const int speedList[] = {-1000, -800, -600, -400, 0, +400, +600, +800, +1000};
+const int speedSize = sizeof(speedList) / sizeof(speedList[0]);
+const int stopIndex = 4;
+int speedIndex = stopIndex;
+int angle = NEUTRAL_ANGLE;
+unsigned long waitTime;
+bool turnBack = false;
+const int GLITCH = 40;  // How much to overturn to compensate for hysteresis
+const int DELAY= 45; // How long to wait for a trun to complete.
+
+Encoder enc(D1,D7);
+Ticker speedSetter;
+WiFiServer server(3000);
+Servo   servo;                               // servo control object
 
 void setup() {  // Setup runs once per reset
-  Serial.begin(115200);
+  pinMode(servoPower, OUTPUT);
+  digitalWrite(servoPower, LOW);
+  pinMode(steerPin, OUTPUT);
+  servo.attach(steerPin, MIN_SERVO, MAX_SERVO);
+  servo.write(angle);
 
+  delay(1000);
   //Define TB6612FNG Dual H-Bridge Motor Controller Pins
-
-  pinMode(dir1PinA, OUTPUT);
+  pinMode(dir1PinA, OUTPUT);  
   pinMode(dir2PinA, OUTPUT);
   pinMode(speedPinA, OUTPUT);
   pinMode(enablePin, OUTPUT);
+  digitalWrite(enablePin, HIGH);
+  digitalWrite(servoPower, HIGH);
+  
+  speedSetter.attach_ms(10, setSpeed);
+
+  int retID = WiFi.softAP(ssid, password);             // Start the access point
+  IPAddress myIP = WiFi.softAPIP();
+  server.begin();
+}
+
+void setSpeed() {
+  unsigned long time = micros(); // Remember to check for overflow.
+  int position = enc.read();
+  if (time > lastTime) {
+    // update speed estimate
+    diffP = position - lastPosition;
+    diffT = time - lastTime;
+    speedEstimate = 10000L * diffP / diffT;
+  }
+  lastTime = time;
+  lastPosition = position;
 }
 
 void loop() {
-
-  // Initialize the Serial interface:
-
-  if (Serial.available() > 0) {
-    int inByte = Serial.read();
-    int speed; // Local variable
-
-    switch (inByte) {
-
-      //______________Motor 1______________
-
-      case '1': // Motor 1 Forward
-        analogWrite(speedPinA, 100);//Sets speed variable via PWM
-        digitalWrite(dir1PinA, LOW);
-        digitalWrite(dir2PinA, HIGH);
-        digitalWrite(enablePin, HIGH);
-        Serial.println("Motor 1 Forward"); // Prints out “Motor 1 Forward” on the serial monitor
-        Serial.println("   "); // Creates a blank line printed on the serial monitor
-        break;
-
-      case '2': // Motor 1 Stop (Brake)
-        analogWrite(speedPinA, 0);
-        digitalWrite(dir1PinA, HIGH);
-        digitalWrite(dir2PinA, HIGH);
-        digitalWrite(enablePin, HIGH);
-        Serial.println("Motor 1 Brake");
-        Serial.println("   ");
-        break;
-
-      case '3': // Motor 1 Reverse
-        analogWrite(speedPinA, 100);
-        digitalWrite(dir1PinA, HIGH);
-        digitalWrite(dir2PinA, LOW);
-        digitalWrite(enablePin, HIGH);
-        Serial.println("Motor 1 Reverse");
-        Serial.println("   ");
-        break;
-
-      case '4': // Motor 1 Coast
-        digitalWrite(enablePin, LOW);
-        digitalWrite(speedPinA, LOW);
-        digitalWrite(dir1PinA, LOW);
-        digitalWrite(dir2PinA, LOW);
-        Serial.println("Motor 1 Coast");
-        Serial.println("   ");
-        break;
-
-      case '5': // Motor 1 Short Brake
-        digitalWrite(speedPinA, HIGH);
-        digitalWrite(dir1PinA, HIGH);
-        digitalWrite(dir2PinA, HIGH);
-        digitalWrite(enablePin, HIGH);
-        Serial.println("Motor 1 Short Brake");
-        Serial.println("   ");
-        break;
-
-      default:
-        break;
+  int inByte = 0;
+  if (!client || !client.connected()) {
+    client = server.available();
+  }
+  if (client) {
+    if (client.connected()) {
+      if (client.available() > 0) {
+        inByte = client.read();
+      }
+    } else {
+      client.stop();
     }
+  }
+
+  if (turnBack) {
+    if (millis() > waitTime + DELAY) {
+      turnBack = false;
+      servo.write(angle);
+    }
+  }
+
+  switch (inByte) {
+    case '+': // Motor 1 Forward
+      speedIndex = min(speedIndex + 1, speedSize);
+      break;
+    case '-': // Motor 1 Stop (Brake)
+      speedIndex = max(speedIndex - 1, 0);
+      break;
+    case '0':
+      speedIndex = stopIndex;
+      break;        
+    case 'L':
+      // always arrive from the same direction (from a higher angle)
+      angle -= 10;;
+      servo.write(angle);
+      break;
+    case 'R':
+      // always arrive from the same direction (from a higher angle)
+      angle += 10;
+      servo.write(angle+GLITCH);
+      waitTime = millis();
+      turnBack = true;
+      break;
+    case 'C':
+      angle = NEUTRAL_ANGLE;
+      servo.write(angle+GLITCH);
+      waitTime = millis();
+      turnBack = true;
+      break;
+    case 'S':
+    case 's':
+      client.print("diffT: ");
+      client.println(diffT);
+      client.print("DiffP: ");
+      client.println(diffP);
+      client.print("EstimatedSPeed: ");
+      client.println(speedEstimate);
+      client.println("   "); // Creates a blank line printed on the client monitor         
+      client.print("Encoder Value");
+      client.println(enc.read());
+      client.print("Speed: ");
+      client.println(speedList[speedIndex]);
+      client.flush();
+      break;
+    default:
+      break;
+  }
+  if (speedList[speedIndex] > 0) {
+    analogWrite(speedPinA, speedList[speedIndex]);//Sets speed variable via PWM
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+  } else if (speedList[speedIndex] < 0) {
+    analogWrite(speedPinA, -1 * speedList[speedIndex]);//Sets speed variable via PWM
+    digitalWrite(dir1PinA, HIGH);
+    digitalWrite(dir2PinA, LOW);
+  } else {
+    analogWrite(speedPinA, 0);//Sets speed variable via PWM
+    digitalWrite(dir1PinA, HIGH);
+    digitalWrite(dir2PinA, HIGH);      
   }
 }
