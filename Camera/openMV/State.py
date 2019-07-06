ecce2df3343b48b4b6826fbd6b34852e271e8eb4
Professor Dave Cohen <d.cohen@rhul.ac.uk>
2019-07-06 09:10:33 +0100

    Added a new message type 'P' to allow the Bluno to send images over to the Camera. These are then always stored in a file called 'IMAGE'. This means that (for example) NFC tags can be displayed as images!

--- Camera/openMV/State.py
+++ Camera/openMV/State.py
@@ -28,8 +28,43 @@ class State:
     def __init__(self):
         self._name = "None"
     def handleChar(self, rdr, recvd):
-        print('Error : NO STATE')
+        #print('Error : NO STATE')
         pass
+
+class StateImageReader(State):
+    def __init__(self):
+        global message
+        self.called = 0;
+        self._name = "BMP Image Reader"
+    def handleChar(self, rdr, recvd):
+        global message
+        # The first two bytes are little endian length of file.
+        if self.called == 0:
+            message.dataCount += recvd
+        else:
+            message.dataCount += 256 * recvd
+        self.called += 1
+        if self.called < 2:
+            return
+        if message.dataCount > 10000:
+            print(self._name + " - got Bad Image Length:" + message.dataCount)
+            message.status = MSG_BAD
+            rdr._state = myStateWaiting
+            return
+        rdr._state = myReadFile
+
+class StateReadFile(State):
+    def __init__(self):
+        self._name = "Image File Reader"
+        self.fl = open("IMAGE", "wb+")
+    def handleChar(self, rdr, recvd):
+        global message
+        self.fl.write(bytes([recvd]))
+        message.dataCount -= 1
+        if message.dataCount > 0:
+            return
+        self.fl.close()
+        rdr._state = myStateEnd
 
 class StateWaiting(State):
     def __init__(self):
@@ -37,8 +72,7 @@ class StateWaiting(State):
     def handleChar(self, rdr, recvd):
         # Simply ignore no-start characters.
         if recvd != START:
-            print(self._name + " - got Bad:" + recvd)
-            message.status = MSG_BAD
+            print(self._name + " - Still Waiting - got:" + str(recvd))
             return
         message.status = MSG_PARTIAL
         rdr._state = myStateCMD
@@ -48,14 +82,19 @@ class StateCMD(State):
         self._name = "CMD"
     def handleChar(self, rdr, recvd):
         global message
-        if not recvd in ['C', 'R', 'L']: # Possible commands
-            print(self._name + " - got Bad:" + recvd)
+        if not recvd in ['C', 'R', 'L', 'P']: # Possible commands
+            print(self._name + " - got Bad:" + str(recvd))
             message.status = MSG_BAD
             rdr._state = myStateWaiting
             return
         # Check CMD validity
         message.status = MSG_PARTIAL
         message.command = recvd
+        message.dataCount = 0
+        if recvd == 'P': # Picture command
+            rdr._state = myImageReader
+            print(self._name + " - got a picture command")
+            return
         rdr._state = myStateCount
 
 class StateCount(State): # count is offset from 64 ('@') - So 'A' == 1
@@ -65,7 +104,7 @@ class StateCount(State): # count is offset from 64 ('@') - So 'A' == 1
         global message
         count = ord(recvd) - ord('@')
         if count > 20:
-            print(self._name + " - got Bad:" + recvd)
+            print(self._name + " - got Bad:" + str(recvd))
             message.status = MSG_BAD
             rdr._state = myStateWaiting
             return
@@ -84,7 +123,7 @@ class StateData(State):
     def handleChar(self, rdr, recvd):
         global message
         if recvd < '0' or recvd > '9':
-            print(self._name + " - got Bad:" + recvd)
+            print(self._name + " - got Bad:" + str(recvd))
             rdr._state = myStateWaiting
             message.status = MSG_BAD
             return
@@ -99,7 +138,7 @@ class StateEnd(State):
     def handleChar(self, rdr, recvd):
         global message
         if recvd != END:
-            print(self._name + " - got Bad:" + recvd)
+            print(self._name + " - got Bad:" + str(recvd))
             message.status = MSG_BAD
         else:
             message.status = MSG_GOOD # Got a whole good message.
@@ -109,8 +148,10 @@ class StateEnd(State):
 myStateWaiting = StateWaiting()
 myStateCMD = StateCMD()
 myStateCount = StateCount()
-myStateData = StateData() 
+myStateData = StateData()
 myStateEnd = StateEnd()
+myImageReader = StateImageReader()
+myReadFile = StateReadFile()
 class Reader:
     """ This is the state pattern to respond to UART tokens
 
--- Camera/openMV/testState.py
+++ Camera/openMV/testState.py
@@ -3,89 +3,107 @@ import unittest
 
 input1 = ['{', 'C', 'D', '1', '2', '3', '4', '}']
 class TestReader(unittest.TestCase):
-	def setUp(self):
-		global rdr
-		rdr = Reader()
-	def test_Wait(self):
-		rdr.handleChar(START)
-		self.assertEqual(message.status, MSG_PARTIAL)
-		self.assertIs(rdr._state, myStateCMD)
-	def test_CMD(self):
-		rdr = Reader()
-		for index in range(2):
-			rdr.handleChar(input1[index])
-		self.assertEqual(message.status, MSG_PARTIAL)
-		self.assertEqual(message.command, 'C')
-		self.assertEqual(rdr._state, myStateCount)
-	def test_Count(self):
-		rdr = Reader()
-		for index in range(3):
-			rdr.handleChar(input1[index])
-		self.assertEqual(message.status, MSG_PARTIAL)
-		self.assertEqual(message.expected, 4)
-		self.assertEqual(rdr._state, myStateData)
-	def test_Data(self):
-		rdr = Reader()
-		for index in range(7):
-			rdr.handleChar(input1[index])
-		self.assertEqual(message.status, MSG_PARTIAL)
-		self.assertEqual(message.dataBuffer[0:message.expected], ['1', '2', '3', '4'])
-		self.assertEqual(rdr._state, myStateEnd)
-	def test_End(self):
-		rdr = Reader()
-		for index in range(8):
-			rdr.handleChar(input1[index])
-		self.assertEqual(message.status, MSG_GOOD)
-		self.assertEqual(rdr._state, myStateWaiting)
-	def test_Wait_Bad(self): # in state Waiting receive a bad input
-		rdr.handleChar('X')
-		self.assertIs(rdr._state, myStateWaiting)
-	def test_CMD_Bad(self): # state CMD - receive bad input
-		rdr = Reader()
-		for index in range(1):
-			rdr.handleChar(input1[index])
-		rdr.handleChar('X')
-		self.assertEqual(message.status, MSG_BAD)
-		self.assertIs(rdr._state, myStateWaiting)
-	def test_Count_Bad(self):
-		rdr = Reader()
-		for index in range(2):
-			rdr.handleChar(input1[index])
-		rdr.handleChar('X')
-		self.assertEqual(message.status, MSG_BAD)
-		self.assertIs(rdr._state, myStateWaiting)
-	def test_Data_Bad(self):
-		rdr = Reader()
-		for index in range(5):
-			rdr.handleChar(input1[index])
-		rdr.handleChar(END)
-		self.assertEqual(message.status, MSG_BAD)
-		self.assertIs(rdr._state, myStateWaiting)
-	def test_End_Bad(self):
-		rdr = Reader()
-		for index in range(7):
-			rdr.handleChar(input1[index])
-		rdr.handleChar('X')
-		self.assertEqual(message.status, MSG_BAD)
-		self.assertEqual(rdr._state, myStateWaiting)
-	def test_Count_Recover(self):
-		rdr = Reader()
-		for index in range(2):
-			rdr.handleChar(input1[index])
-		rdr.handleChar('X')
-		for index in range(8):
-			rdr.handleChar(input1[index])
-		self.assertEqual(message.status, MSG_GOOD)
-		self.assertIs(rdr._state, myStateWaiting)
-	def test_NOArgs(self):
-		rdr = Reader()
-		rdr.handleChar('{')
-		rdr.handleChar('L')
-		rdr.handleChar('@')
-		rdr.handleChar('}')
-		self.assertEqual(message.status, MSG_GOOD)
-		self.assertIs(rdr._state, myStateWaiting)
-
+    def setUp(self):
+        global rdr
+        rdr = Reader()
+    def test_Wait(self):
+        rdr.handleChar(START)
+        self.assertEqual(message.status, MSG_PARTIAL)
+        self.assertIs(rdr._state, myStateCMD)
+    def test_CMD(self):
+        rdr = Reader()
+        for index in range(2):
+            rdr.handleChar(input1[index])
+        self.assertEqual(message.status, MSG_PARTIAL)
+        self.assertEqual(message.command, 'C')
+        self.assertEqual(rdr._state, myStateCount)
+    def test_Count(self):
+        rdr = Reader()
+        for index in range(3):
+            rdr.handleChar(input1[index])
+        self.assertEqual(message.status, MSG_PARTIAL)
+        self.assertEqual(message.expected, 4)
+        self.assertEqual(rdr._state, myStateData)
+    def test_Data(self):
+        rdr = Reader()
+        for index in range(7):
+            rdr.handleChar(input1[index])
+        self.assertEqual(message.status, MSG_PARTIAL)
+        self.assertEqual(message.dataBuffer[0:message.expected], ['1', '2', '3', '4'])
+        self.assertEqual(rdr._state, myStateEnd)
+    def test_End(self):
+        rdr = Reader()
+        for index in range(8):
+            rdr.handleChar(input1[index])
+        self.assertEqual(message.status, MSG_GOOD)
+        self.assertEqual(rdr._state, myStateWaiting)
+    def test_Wait_Bad(self): # in state Waiting receive a bad input
+        rdr.handleChar('X')
+        self.assertIs(rdr._state, myStateWaiting)
+    def test_CMD_Bad(self): # state CMD - receive bad input
+        rdr = Reader()
+        for index in range(1):
+            rdr.handleChar(input1[index])
+        rdr.handleChar('X')
+        self.assertEqual(message.status, MSG_BAD)
+        self.assertIs(rdr._state, myStateWaiting)
+    def test_Count_Bad(self):
+        rdr = Reader()
+        for index in range(2):
+            rdr.handleChar(input1[index])
+        rdr.handleChar('X')
+        self.assertEqual(message.status, MSG_BAD)
+        self.assertIs(rdr._state, myStateWaiting)
+    def test_Data_Bad(self):
+        rdr = Reader()
+        for index in range(5):
+            rdr.handleChar(input1[index])
+        rdr.handleChar(END)
+        self.assertEqual(message.status, MSG_BAD)
+        self.assertIs(rdr._state, myStateWaiting)
+    def test_End_Bad(self):
+        rdr = Reader()
+        for index in range(7):
+            rdr.handleChar(input1[index])
+        rdr.handleChar('X')
+        self.assertEqual(message.status, MSG_BAD)
+        self.assertEqual(rdr._state, myStateWaiting)
+    def test_Count_Recover(self):
+        rdr = Reader()
+        for index in range(2):
+            rdr.handleChar(input1[index])
+        rdr.handleChar('X')
+        for index in range(8):
+            rdr.handleChar(input1[index])
+        self.assertEqual(message.status, MSG_GOOD)
+        self.assertIs(rdr._state, myStateWaiting)
+    def test_NOArgs(self):
+        rdr = Reader()
+        rdr.handleChar('{')
+        rdr.handleChar('L')
+        rdr.handleChar('@')
+        rdr.handleChar('}')
+        self.assertEqual(message.status, MSG_GOOD)
+        self.assertIs(rdr._state, myStateWaiting)
+    def test_Image(self):
+        fl = open("testImage", "rb")
+        img = fl.read()
+        fl.close()
+        rdr = Reader()
+        rdr.handleChar('{')
+        bCount = len(img).to_bytes(2, byteorder='little')
+        rdr.handleChar('P')
+        rdr.handleChar(bCount[0])
+        rdr.handleChar(bCount[1])
+        for index in range(len(img)):
+            rdr.handleChar(img[index])
+        rdr.handleChar('}')
+        fl = open("IMAGE", "rb")
+        imgNew = fl.read()
+        fl.close()
+        self.assertEqual(message.status, MSG_GOOD)
+        self.assertIs(rdr._state, myStateWaiting)
+        self.assertSequenceEqual(imgNew, img)
 
 if __name__ == '__main__':
     unittest.main()
