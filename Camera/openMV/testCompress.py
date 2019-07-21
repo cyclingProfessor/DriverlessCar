cec42284460f9a022b9eaea023a2e91110f1192f
Professor Dave Cohen <d.cohen@rhul.ac.uk>
2019-07-21 17:05:50 +0100

    New unit testing after better memory managements and other refactorings

--- Camera/openMV/testCompress.py
+++ Camera/openMV/testCompress.py
@@ -1,19 +1,22 @@
-from Compress import *
+from CompressUtil import *
 from random import seed, randrange
 import unittest
 
 class TestReader(unittest.TestCase):
     def test_Simple(self):
-        data = b"I am a string"
+        data = b"ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
         code = compress(data)
-        uncompressed = decompress(code)
+ #       print(bin(int.from_bytes(code, 'big')))
+        uncompressed = bytearray(len(data))
+        decompress(code, uncompressed)
         self.assertSequenceEqual(data, uncompressed)
 
     def test_Random(self):
         seed(0)
         data = bytes(randrange(255) for i in range(10000))
         code = compress(data)
-        uncompressed = decompress(code)
+        uncompressed = bytearray(len(data))
+        decompress(code, uncompressed)
         self.assertSequenceEqual(data, uncompressed)
 
     def test_Image(self):
@@ -21,7 +24,8 @@ class TestReader(unittest.TestCase):
         data = fp.read()
         fp.close()
         code = compress(data)
-        uncompressed = decompress(code)
+        uncompressed = bytearray(len(data))
+        decompress(code, uncompressed)
         self.assertSequenceEqual(data, uncompressed)
 
     def test_Escape(self):
@@ -29,11 +33,22 @@ class TestReader(unittest.TestCase):
         data += bytes(range(256))
         self.assertIn(b"{|}", data)
         code = compress(data)
+#        print(code)
+#        print(bin(int.from_bytes(code, 'big')))
         self.assertNotIn(b"{", code)
         self.assertNotIn(b"}", code)
         self.assertEqual(code.count(b"|"), 2)
-        uncompressed = decompress(code)
+        uncompressed = bytearray(len(data))
+        decompress(code, uncompressed)
         self.assertSequenceEqual(data, uncompressed)
+
+    def test_RGB565(self):
+        seed(0)
+        data = bytes(randrange(255) for i in range(3333))
+        code = compress(data)
+        image = [None] * 1111
+        decompressImage(code, image)
+        self.assertSequenceEqual(data, [item for sublist in image for item in sublist])
 
 if __name__ == '__main__':
     unittest.main()
--- Camera/openMV/testState.py
+++ Camera/openMV/testState.py
@@ -1,5 +1,6 @@
 from State import *
 import unittest
+from CompressUtil import compress
 
 input1 = ['{', 'C', 'D', '1', '2', '3', '4', '}']
 class TestReader(unittest.TestCase):
@@ -8,34 +9,34 @@ class TestReader(unittest.TestCase):
         rdr = Reader()
     def test_Wait(self):
         rdr.handleChar(START)
-        self.assertEqual(message.status, MSG_PARTIAL)
+        self.assertEqual(rdr.getStatus(), MSG_PARTIAL)
         self.assertIs(rdr._state, myStateCMD)
     def test_CMD(self):
         rdr = Reader()
         for index in range(2):
             rdr.handleChar(input1[index])
-        self.assertEqual(message.status, MSG_PARTIAL)
-        self.assertEqual(message.command, 'C')
+        self.assertEqual(rdr.getStatus(), MSG_PARTIAL)
+        self.assertEqual(rdr._state.message.command, 'C')
         self.assertEqual(rdr._state, myStateCount)
     def test_Count(self):
         rdr = Reader()
         for index in range(3):
             rdr.handleChar(input1[index])
-        self.assertEqual(message.status, MSG_PARTIAL)
-        self.assertEqual(message.expected, 4)
+        self.assertEqual(rdr.getStatus(), MSG_PARTIAL)
+        self.assertEqual(rdr._state.message.expected, 4)
         self.assertEqual(rdr._state, myStateData)
     def test_Data(self):
         rdr = Reader()
         for index in range(7):
             rdr.handleChar(input1[index])
-        self.assertEqual(message.status, MSG_PARTIAL)
-        self.assertEqual(message.dataBuffer[0:message.expected], ['1', '2', '3', '4'])
+        self.assertEqual(rdr.getStatus(), MSG_PARTIAL)
+        self.assertEqual(rdr._state.message.dataBuffer[0:rdr._state.message.expected], ['1', '2', '3', '4'])
         self.assertEqual(rdr._state, myStateEnd)
     def test_End(self):
         rdr = Reader()
         for index in range(8):
             rdr.handleChar(input1[index])
-        self.assertEqual(message.status, MSG_GOOD)
+        self.assertEqual(rdr.getStatus(), MSG_GOOD)
         self.assertEqual(rdr._state, myStateWaiting)
     def test_Wait_Bad(self): # in state Waiting receive a bad input
         rdr.handleChar('X')
@@ -45,28 +46,28 @@ class TestReader(unittest.TestCase):
         for index in range(1):
             rdr.handleChar(input1[index])
         rdr.handleChar('X')
-        self.assertEqual(message.status, MSG_BAD)
+        self.assertEqual(rdr.getStatus(), MSG_BAD)
         self.assertIs(rdr._state, myStateWaiting)
     def test_Count_Bad(self):
         rdr = Reader()
         for index in range(2):
             rdr.handleChar(input1[index])
         rdr.handleChar('X')
-        self.assertEqual(message.status, MSG_BAD)
+        self.assertEqual(rdr.getStatus(), MSG_BAD)
         self.assertIs(rdr._state, myStateWaiting)
     def test_Data_Bad(self):
         rdr = Reader()
         for index in range(5):
             rdr.handleChar(input1[index])
         rdr.handleChar(END)
-        self.assertEqual(message.status, MSG_BAD)
+        self.assertEqual(rdr.getStatus(), MSG_BAD)
         self.assertIs(rdr._state, myStateWaiting)
     def test_End_Bad(self):
         rdr = Reader()
         for index in range(7):
             rdr.handleChar(input1[index])
         rdr.handleChar('X')
-        self.assertEqual(message.status, MSG_BAD)
+        self.assertEqual(rdr.getStatus(), MSG_GOOD)
         self.assertEqual(rdr._state, myStateWaiting)
     def test_Count_Recover(self):
         rdr = Reader()
@@ -75,7 +76,7 @@ class TestReader(unittest.TestCase):
         rdr.handleChar('X')
         for index in range(8):
             rdr.handleChar(input1[index])
-        self.assertEqual(message.status, MSG_GOOD)
+        self.assertEqual(rdr.getStatus(), MSG_GOOD)
         self.assertIs(rdr._state, myStateWaiting)
     def test_NOArgs(self):
         rdr = Reader()
@@ -83,24 +84,24 @@ class TestReader(unittest.TestCase):
         rdr.handleChar('L')
         rdr.handleChar('@')
         rdr.handleChar('}')
-        self.assertEqual(message.status, MSG_GOOD)
+        self.assertEqual(rdr.getStatus(), MSG_GOOD)
         self.assertIs(rdr._state, myStateWaiting)
     def test_Image(self):
         rdr = Reader()
         rdr.handleChar('{')
         rdr.handleChar('P')
-        rdr.handleChar(chr(44))
-        rdr.handleChar(chr(33))
-        for index in range(44 * 33):
-            rdr.handleChar(chr(254))
-            rdr.handleChar(chr(124))
-            rdr.handleChar(chr(0))
+        data = bytes((index % 7) for index in range(3000))
+        code = compress(data)
+        rdr.handleChar(chr(len(code) >> 6 & 0x3F))
+        rdr.handleChar(chr(len(code) & 0x3F))
+        for value in code:
+            rdr.handleChar(chr(value))
         rdr.handleChar('}')
-        self.assertEqual(message.status, MSG_GOOD)
+        self.assertEqual(rdr.getStatus(), MSG_GOOD)
         self.assertIs(rdr._state, myStateWaiting)
-        self.assertEqual(message.picture.width(), 44)
-        self.assertEqual(message.picture.height(), 33)
-        self.assertSequenceEqual(message.picture[199], (254, 124, 0))
+        pixels = list(zip(data[::3], data[1::3], data[2::3]))
+        for index in range(1000):
+            self.assertSequenceEqual(rdr._state.message.picture[index], pixels[index])
 
 if __name__ == '__main__':
     unittest.main()
