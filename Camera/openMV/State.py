b6a0ef8ac6eb62560c8954ce010c0d56eadd91ac
Professor Dave Cohen <d.cohen@rhul.ac.uk>
2019-07-21 17:00:23 +0100

    Moved the Message object into the structures so that we no longer need global - and provided accessor methods in Reader to access important status info. Also supported the new fixed cost image procesing uncompress utility.

--- Camera/openMV/State.py
+++ Camera/openMV/State.py
@@ -1,4 +1,5 @@
 import image, sensor
+from CompressUtil import decompressImage
 
 START = '{' # 123
 END = '}' # 125
@@ -18,16 +19,17 @@ class Message:
         self.status = MSG_NONE # -2 for None yet, -1 for error, 0 for partial, 1 for complete
         self.dataCount = 0
         self.expected = -1
-        self.picture = None
+        self.picture = image.Image(60,40,sensor.RGB565)
+        self.decompressTable = bytearray(3000)
 
 ######################################################################################
-message = Message()
 
 class State:
     """
     Define an interface for encapsulating the behavior associated with a
     particular state of the Context.
     """
+    message = Message() # Single class variable
     def __init__(self):
         self._name = "None"
     def handleChar(self, rdr, recvd):
@@ -36,80 +38,77 @@ class State:
 
 class StateImageReader(State):
     def __init__(self):
-        global message
-        self.called = False;
+        super().__init__()
+        self.started = False;
         self._name = "Image Reader"
         self.width = 0
     def handleChar(self, rdr, recvd):
-        global message
-        # The first two bytes are Row and Column counts.
-        if not self.called:
-            self.width = ord(recvd) # Number of Columns
-            self.called = True
-            return
-        message.picture = image.Image(self.width, ord(recvd), sensor.RGB565)
-        # Pixel values are all even as a simple error check.
-        # Of course start (123) and end (125) now cannot occur in an image
-        message.expected = self.width * ord(recvd) # Number of Pixels (each is three numbers)
-        message.dataCount = 0;
-        self.called = False
-
-        print("Expecting : " + str(message.expected))
-        if message.expected > 10000:
-            print(self._name + " - got Bad Image Size:" + str(message.expected))
-            message.status = MSG_BAD
+        if ord(recvd) > 100:
+            self.started = False;
+            print(self._name + " - got Bad:" + str(recvd))
+            self.message.status = MSG_BAD
             rdr._state = myStateWaiting
             return
+        # The first two bytes are big endian 6-bit Data (compressed) counts.
+        if not self.started == 0:
+            self.message.expected = ord(recvd)
+            self.started = True
+            return
+
+        self.message.expected = 64 * self.message.expected + ord(recvd)
+        self.message.dataCount = 0
+        self.started = False
+
+        print("Expecting : " + str(self.message.expected))
+        if self.message.expected > 1000: # Size of decode table
+            print(self._name + " - got Bad Image Size:" + str(self.message.expected))
+            self.message.status = MSG_BAD
+            rdr._state = myStateWaiting
+            return
+        decompressImageInit(self.message.picture, self.message.decompressTable)
         rdr._state = myReadPixels
 
 class StateReadPixels(State):
     def __init__(self):
+        super().__init__()
         self._name = "Pixel Reader"
-        self.colorIndex = 0
-        self.color = [None, None, None]
     def handleChar(self, rdr, recvd):
-        global message
-        if ord(recvd) % 2 == 1:
-            self.colorIndex = 0;
+        if recvd in [END, START]: # Only two bad characters! premature!
             print(self._name + " - got Bad:" + str(recvd))
-            message.status = MSG_BAD
+            self.message.status = MSG_BAD
             rdr._state = myStateWaiting
             return
-        self.color[self.colorIndex] = ord(recvd)
-        self.colorIndex = self.colorIndex + 1
-        if self.colorIndex < 3:
-            return
-        self.colorIndex = 0
-        message.picture[message.dataCount] = self.color
-        message.dataCount += 1
-        if message.dataCount == message.expected:
+        decompressImageProcess(recvd)
+        self.message.dataCount += 1
+        if self.message.dataCount == self.message.expected:
             rdr._state = myStateEnd
 
 class StateWaiting(State):
     def __init__(self):
+        super().__init__()
         self._name = "Waiting"
     def handleChar(self, rdr, recvd):
         # Simply ignore no-start characters.
         if recvd != START:
             print(self._name + " - Still Waiting - got:" + str(recvd))
             return
-        message.status = MSG_PARTIAL
+        self.message.status = MSG_PARTIAL
         rdr._state = myStateCMD
 
 class StateCMD(State):
     def __init__(self):
+        super().__init__()
         self._name = "CMD"
     def handleChar(self, rdr, recvd):
-        global message
         if not recvd in ['C', 'R', 'L', 'P']: # Possible commands
             print(self._name + " - got Bad:" + str(recvd))
-            message.status = MSG_BAD
+            self.message.status = MSG_BAD
             rdr._state = myStateWaiting
             return
         # Check CMD validity
-        message.status = MSG_PARTIAL
-        message.command = recvd
-        message.dataCount = 0
+        self.message.status = MSG_PARTIAL
+        self.message.command = recvd
+        self.message.dataCount = 0
         if recvd == 'P': # Picture command
             rdr._state = myImageReader
             print(self._name + " - got a picture command")
@@ -118,49 +117,49 @@ class StateCMD(State):
 
 class StateCount(State): # count is offset from 64 ('@') - So 'A' == 1
     def __init__(self):
+        super().__init__()
         self._name = "Count"
     def handleChar(self, rdr, recvd):
-        global message
         count = ord(recvd) - ord('@')
         if count > 20:
             print(self._name + " - got Bad:" + str(recvd))
-            message.status = MSG_BAD
+            self.message.status = MSG_BAD
             rdr._state = myStateWaiting
             return
         # Check Count validity (Certainly < 20)
         # If expected = 0 then straight to END
-        message.expected = count
-        message.dataCount = 0
-        if message.expected == 0:
+        self.message.expected = count
+        self.message.dataCount = 0
+        if self.message.expected == 0:
             rdr._state = myStateEnd
         else:
             rdr._state = myStateData
 
 class StateData(State):
     def __init__(self):
+        super().__init__()
         self._name = "Data"
     def handleChar(self, rdr, recvd):
-        global message
         if recvd < '0' or recvd > '9':
             print(self._name + " - got Bad:" + str(recvd))
             rdr._state = myStateWaiting
-            message.status = MSG_BAD
+            self.message.status = MSG_BAD
             return
-        message.dataBuffer[message.dataCount] = recvd
-        message.dataCount += 1
-        if message.dataCount >= message.expected:
+        self.message.dataBuffer[self.message.dataCount] = recvd
+        self.message.dataCount += 1
+        if self.message.dataCount >= self.message.expected:
             rdr._state = myStateEnd
 
 class StateEnd(State):
     def __init__(self):
+        super().__init__()
         self._name = "End"
     def handleChar(self, rdr, recvd):
-        global message
         if recvd != END:
             print(self._name + " - got Bad:" + str(recvd))
-            message.status = MSG_BAD
+            self.message.status = MSG_BAD
         else:
-            message.status = MSG_GOOD # Got a whole good message.
+            self.message.status = MSG_GOOD # Got a whole good self.message.
         rdr._state = myStateWaiting
 
 ## SInce states are stateless (sic) we can re-use them
@@ -178,6 +177,9 @@ class Reader:
     """
     def __init__(self):
         self._state = myStateWaiting
+
+    def getStatus(self):
+        return self._state.message.status
 
     def handleChar(self, recvd):
         self._state.handleChar(self, recvd)
